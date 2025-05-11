2acd71e226e9598fd79430d097c6d6005784690e
Ryon McAuley <rdmcauley@gmail.com>
2025-05-11 17:40:15 -0400

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -17,15 +17,12 @@ fn command_prompt() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
 
-    // return anything as invalid command
     let (resp, _) = switch_command(input.trim());
     if resp == "exit" {
         return;
     } else {
-        // println!("u doin sum");
-        // println!("{}: {}", resp, input.trim());
         println!("{}", resp.trim());
-        // Call the command prompt again
+        // Keep command prompt up until exit
         command_prompt();
     }
 }
@@ -35,9 +32,7 @@ fn switch_command(input: &str) -> (String, bool) {
     if input.is_empty() {
         return (String::from(""), builtin_command_flag);
     }
-    // let mut input_iter = input.split_whitespace();
     let parts = input.split_whitespace().collect::<Vec<&str>>();
-    // let command = input_iter.next().unwrap_or("");
     let command = parts[0];
     match command {
         "exit" => {
@@ -46,7 +41,6 @@ fn switch_command(input: &str) -> (String, bool) {
         }
         "echo" => {
             // return the rest of the input
-            // return input_iter.collect::<Vec<&str>>().join(" ");
             builtin_command_flag = true;
             return (parts[1..].join(" "), builtin_command_flag);
         }
@@ -81,36 +75,17 @@ fn switch_command(input: &str) -> (String, bool) {
             // return invalid if command is not found
             return (format!("{}: not found", query), builtin_command_flag);
         }
-        // "help" => {
-        //     println!("Available commands: help, exit 0");
-        // }
         _ => {
             if find_file_in_path(command).is_some() {
                 // trim input
                 let output = Command::new(command).args(&parts[1..]).output();
-                // not my solution
-                // match std::process::Command::new(command)
-                //     .args(&parts[1..])
-                //     .output()
-                // {
-                //     Ok(output) => {
-                //         // io::stdout().write_all(&output.stdout).unwrap();
-                //         // io::stderr().write_all(&output.stderr).unwrap();
-                //         return String::from_utf8_lossy(&output.stdout).to_string();
-                //     }
-                //     Err(_) => {
-                //         // eprintln!("{}: command not found", command);
-                //         return format!("{}: command not found", command);
-                //     }
-                // }
                 let output_stdout = output
                     .unwrap()
                     .stdout
                     .iter()
                     .map(|&b| b as char)
                     .collect::<String>();
-                // // println!("Stdout: {}", output_stdout);
-                // // return executable output
+                // return executable output
                 return (output_stdout.trim().to_string(), builtin_command_flag);
             } else {
                 return (
@@ -123,24 +98,14 @@ fn switch_command(input: &str) -> (String, bool) {
 }
 
 fn is_builtin(command: &str) -> bool {
+    // return the is_builtin flag for the queried command, inefficient but should be reliable
     return switch_command(command).1;
-    // try running command, if it fails, return invalid
-    // let resp = switch_command(command);
-    // // if not found or is in PATH, return false
-    // if resp.contains("not found") || resp.contains(&format!("{} is", command)) || resp.is_empty() {
-    //     return false;
-    // }
-    // return true;
-
-    // let builtins = ["echo", "type", "exit"];
-    // builtins.contains(&command)
 }
 
 fn find_file_in_path(file_name: &str) -> Option<String> {
     let path = var("PATH").unwrap_or_default();
     let paths = path.split(':');
     for p in paths {
-        // file_path = path / query
         let file_path = format!("{}/{}", p, file_name);
         if Path::new(&file_path).exists() {
             return Some(file_path);
